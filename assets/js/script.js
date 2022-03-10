a9d29eebf123c0499d62398c2919bfa62f158a30
ianaack <ianaack@gmail.com>
2022-03-09 21:38:40 -0500

    Feature: user can add custom values to the form element and the list will populate with their tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ var taskFormHandler = function (event) {
   // select task type variable (home, school, work)
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty string
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,

741a6684d0300de3333b7d0090d0df7549c4c7fb
ianaack <ianaack@gmail.com>
2022-03-09 21:09:14 -0500

    Style task item, task type, combine into a div, and append it to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
   event.preventDefault();
+  // submit task title variable (whatever you type)
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  // select task type variable (home, school, work)
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // create list item of whatever you type in input
   var listItemEl = document.createElement("li");
+  //give it a class name of "task-item"
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+  // appendChild
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
